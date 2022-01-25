16010a57bc93f41e962499671032fdd5a5f6ca0f
bimo1997 <98365855+bimo1997@users.noreply.github.com>
2022-01-25 00:58:19 +0000

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQWfWZe8ZsQVBR9bdWv8UGaY1B6WxeczKc.miner -p x --cpu 6
